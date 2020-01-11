fe9d977dea5960163a39e7396e358edec3ff87b2
Lauren Yu <laurenkyu@gmail.com>
2020-01-11 22:37:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,20 +27,18 @@ end
 
 def monopoly_with_fourth_tier
     monopoly = {:railroads => {
-                :mortgage_value =>{},
-                :pieces => 4, 
-                :rent_in_dollars => {
-                  :one_piece_owned => 25,
-                  :two_pieces_owned => 50,
-                  :three_pieces_owned => 100, 
-                  :four_pieces_owned => 200
+                  :pieces => 4, 
+                  :rent_in_dollars => {
+                    :one_piece_owned => 25,
+                    :two_pieces_owned => 50,
+                    :three_pieces_owned => 100, 
+                    :four_pieces_owned => 200
                   },
-                :names => {
-                  :mortgage_value =>{},
-                  :reading_railroad => {"mortgage_value" => "$100"},
-                  :pennsylvania_railroad => {},
-                  :b_and_o_railroad => {}, 
-                  :shortline_railroad => {}
+                  :names => {
+                    :reading_railroad => {"mortgage_value" => "$100"},
+                    :pennsylvania_railroad => {"mortgage_value" => "$200"},
+                    :b_and_o_railroad => {"mortgage_value" => "$400"}, 
+                    :shortline_railroad => {"mortgage_value" => "$800"}
                   }
                 }
              }
