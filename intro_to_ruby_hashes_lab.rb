209f274af6c84c8891992645b59f4537c4a0b098
Lauren Yu <laurenkyu@gmail.com>
2020-01-11 22:35:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,7 +37,7 @@ def monopoly_with_fourth_tier
                   },
                 :names => {
                   :mortgage_value =>{},
-                  :reading_railroad => {},
+                  :reading_railroad => {"mortgage_value" => "$100"},
                   :pennsylvania_railroad => {},
                   :b_and_o_railroad => {}, 
                   :shortline_railroad => {}
