9bccf6bbfd5c64d36a229adc48d0254a6f466427
Lauren Yu <laurenkyu@gmail.com>
2020-01-11 22:22:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,8 +7,16 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  monopoly = {:railroads => {:pieces => 4, :rent_in_dollars => {1, 2, 3, 4}}
-  }
+  monopoly = {:railroads => {
+                :pieces => 4, 
+                :rent_in_dollars => {
+                  :one => {},
+                  :two => {},
+                  :three => {}, 
+                  :four => {}
+        
+                }
+             }
 end
 
 def monopoly_with_fourth_tier
